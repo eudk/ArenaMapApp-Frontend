a6cea1f9ea03061a05518fb09470fd7a0152ad7c
thom803m <115149062+thom803m@users.noreply.github.com>
2024-12-17 00:01:09 +0100

    Changes the structure and updated the floor pages

---
+++ src/js/floor.js
@@ -0,0 +1,6 @@
+// floor.js
+document.addEventListener("DOMContentLoaded", function() {
+    // Dette script kan bruges til at tilføje funktionalitet senere
+    // F.eks. du kan tilføje en funktion til at håndtere klik events på billederne (hvis nødvendigt).
+    console.log("Etage-siden er indlæst.");
+});
